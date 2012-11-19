8413b60f5a3ec647b2e01340730408a06c0ec14e
unknown <mikko.tikkanen@gmail.com>
2012-11-19 16:25:51 +0200

    Changed the animation and pushState around

--- jquery.enhanceanchors.js
+++ jquery.enhanceanchors.js
@@ -18,15 +18,15 @@
 			$el.click(function(event) {
 				event.preventDefault();
 				
-				// Add history with pushState
-				if(window.history && window.history.pushState) {
-					window.history.pushState({}, document.title, $(this).attr('href'));
-				}
-				
 				// Scroll to element
 				$('html, body').animate({
 			         scrollTop: $($(this).attr('href')).offset().top
 			    }, 500);
+			    
+				// Add history with pushState
+				if(window.history && window.history.pushState) {
+					window.history.pushState({}, document.title, $(this).attr('href'));
+				}
 			});
 		});
 	
